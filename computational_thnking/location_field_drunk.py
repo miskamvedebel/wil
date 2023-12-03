6b3561605f712b7f90d27bc3d794f18f8ee42ac4
Maksim Lebedev <Maksim.lebedev@bolt.eu>
2023-12-03 19:31:06 +0100

    solving next week

---
+++ computational_thnking/location_field_drunk.py
@@ -0,0 +1,47 @@
+class Location(object):
+    def __init__(self, x, y):
+        """x, y are floats"""
+
+        self.x = x
+        self.y = y
+
+    def move(self, deltaX, deltaY):
+        """deltaX and deltaY are floats"""
+        return Location(self.x + deltaX, self.y + deltaY)
+
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+
+    def distFrom(self, other):
+        ox = other.x
+        oy = other.y
+        xDist = self.x - ox
+        yDist = self.y - oy
+        return (xDist ** 2 + yDist ** 2) ** 0.5
+
+    def __str__(self):
+        return '<' + str(self.x) + ', ' + str(self.y) + '>'
+
+class Field(object):
+    def __init__(self):
+        self.drunks = {}
+
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk')
+        else:
+            self.drunks[drunk] = loc
+
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
+
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        xDist, yDist = drunk.takeStep()
+        currentLocation = self.drunks[drunk]
+        self.drunks[drunk] = currentLocation.move(xDist, yDist)
